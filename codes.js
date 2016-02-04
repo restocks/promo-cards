12b5a28c51a455a77c362f61355b30a487a3fb9e
Luke Miles <luke@lukemil.es>
2016-02-04 14:36:10 -0800

    add gm code to add codes to card images

--- codes.js
+++ codes.js
@@ -3,17 +3,32 @@ console.log("Starting database");
 var config = require("./config");
 var massive = require("massive");
 var async = require("async");
-var db = massive.connectSync({connectionString : config('pg_url')}); 
+var gm = require("gm");
+var db = massive.connectSync({connectionString : config('pg_url')}) 
 console.timeEnd("Start database");
 function createCode() {
 	// http://stackoverflow.com/questions/1349404/generate-a-string-of-5-random-characters-in-javascript
 	return (Math.random().toString(36)+'00000000000000000').slice(2, 5+2).toUpperCase()
 }
 
-function generateAndSaveCode(number, callback) {
-	db.codes.save({code: createCode()}, function(err) {
+function codeMaker(number, callback) {
+	var code = createCode()
+	db.codes.save({code: code}, function(err) {
 		callback();
 	});
+	generateImageWithCode(code);
+}
+
+function generateImageWithCode(code) {
+	gm("./art/back-master.png").fill('#FFFFFF')
+	    .drawText(0, 515, code, 'Center')
+	    .font('./art/CircularStd-Bold.otf')
+	    .fontSize( '180px' )
+	    .write( "cards/" + code + ".png", function (err) {
+	        if (err) {
+	            throw err;
+	        } 
+	    });
 }
 
 function doneWithCodes() {
@@ -21,4 +36,4 @@ function doneWithCodes() {
 }
 
 // generates as many codes 
-async.times(config("numberOfCodes"), generateAndSaveCode, doneWithCodes);
+async.times(config("numberOfCodes"), codeMaker, doneWithCodes);
